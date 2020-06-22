32e5badac314c5520ab26267b9bdb1d62d38d3f9
codwerk <louis_widdy@live.fr>
2020-06-22 14:11:17 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div id="title">Hello World</div>
+  </div>
+</template>
+
+<script>
+
+  import { gsap } from "gsap";
+
+export default {
+  mounted : function(){
+    this.start()
+  },
+  methods: {
+    start() {
+      gsap.to('#title', {duration: 1, color: "red"})
+    }
+  }
+}
+</script>
+
+<style>
+  #title {
+    width: 350px;
+    height: 350px;
+  }
+</style>
